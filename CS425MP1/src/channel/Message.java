9f190c59cfa5b1c249db9b974bde253065e091eb
ChenXiaoyu <chenxiaoyu@wirelessprvnat-172-17-224-115.near.illinois.edu>
2015-02-15 18:09:15 -0600

    message for today

--- CS425MP1/src/channel/Message.java
+++ CS425MP1/src/channel/Message.java
@@ -1,4 +1,7 @@
 package channel;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -38,7 +41,19 @@ public class Message {
 	 */
 	public int Operation;
 	
-	
+	public void transformForCoordinator()
+	{
+		if(this.Operation>=5&&this.Operation<=8)
+		{
+			this.To=this.From;
+			this.From='O';
+			this.Operation-=4;
+		}
+	}
+	public Message()
+	{
+		
+	}
 	public Message(String c)
 	{
 		String[] parseResult = c.split("\\s+");
@@ -46,7 +61,7 @@ public class Message {
 		op=op.toLowerCase();
 		switch(op)
 		{
-		case "send": this.Operation=0;break;
+		case "send": this.Operation=0;this.Content=parseResult[1];this.To=parseResult[2].charAt(0);break;
 		case "delete": this.Operation=5; this.Key=parseResult[1];this.Model = Integer.parseInt(parseResult[2]); break;
 		case "get": this.Operation=6; this.Key=parseResult[1];this.Value=parseResult[2]; this.Model = Integer.parseInt(parseResult[3]); break;
 		case "insert": this.Operation=7; this.Key=parseResult[1];this.Value=parseResult[2];this.Model = Integer.parseInt(parseResult[3]); break;
@@ -56,6 +71,40 @@ public class Message {
 		case "show-all": this.Operation=12;break;
 		default: System.out.println("WRONG FORMAT OF INPUT!");
 		}
+		
+	}
+	public String messageToString()
+	{
+		StringBuilder sb=new StringBuilder();
+		sb.append(Content).append(";");
+		sb.append(Key).append(";");
+		sb.append(Value).append(";");
+		sb.append(From).append(";");
+		sb.append(To).append(";");
+		sb.append(MaxDelay).append(";");
+		sb.append(Operation).append(";");
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		sb.append(formatter.format(SentTime)).append(";");
+		sb.append(formatter.format(EstimatedDeliverTime)).append(";");
+		sb.append(formatter.format(RealDeliverTime));
+		return sb.toString();
+	}
+	public Message stringToMessage(String s) throws ParseException
+	{
+		String[] ss=s.split(";");
+		Message m=new Message();
+		m.Content=ss[0];
+		m.Key=ss[1];
+		m.Value=ss[2];
+		m.From=ss[3].charAt(0);
+		m.To=ss[4].charAt(0);
+		m.MaxDelay=Integer.parseInt(ss[5]);
+		m.Operation=Integer.parseInt(ss[6]);
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		m.SentTime=formatter.parse(ss[7]);
+		m.EstimatedDeliverTime=formatter.parse(ss[8]);
+		m.RealDeliverTime=formatter.parse(ss[9]);
+		return m;
 		
 	}
 	public void setSentTime()
@@ -75,12 +124,13 @@ public class Message {
 	{
 		RealDeliverTime=new Date();
 	}
-	public static void main(String[] args)
+	public static void main(String[] args) throws ParseException
 	{
-		Message m=new Message("Hello");
+		Message m=new Message("Send hello A");
 		m.setSentTime();
-		System.out.println(m.SentTime.getSeconds());
 		m.setEstimatedDeliverTime(10);
-		System.out.println(m.EstimatedDeliverTime.getSeconds());
+		m.setRealDeliverTime();
+		System.out.println(m.messageToString());
+		System.out.println(m.stringToMessage( m.messageToString()).messageToString());
 	}
 }
