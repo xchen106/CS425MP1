d4bf1d9a7e44c3c496bdb08bb2e64e62a85a6adf
ChenXiaoyu <chenxiaoyu@wirelessprvnat-172-17-8-82.near.illinois.edu>
2015-03-01 12:40:44 -0600

    Debug Repair

--- CS425MP1/src/node/Repair.java
+++ CS425MP1/src/node/Repair.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Repair extends Node
 {
@@ -36,11 +37,15 @@ public class Repair extends Node
 	{
 		for(String kvhashmap: kvarray)
 		{
-			String[] kvs=kvhashmap.split("|");
-			for(int i=0;i<kvs.length-1;i++)
+			String[] kvs=kvhashmap.split("\\|");
+			if(kvs[0].length()<=0)
+				continue;
+			for(int i=0;i<kvs.length;i++)
 			{
 				String kv=kvs[i];
 				String[] elements=kv.split(";");
+				if(elements[0].length()<=0)
+					continue;
 				String key=elements[0];
 				String v=elements[1];
 				DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -51,6 +56,31 @@ public class Repair extends Node
 					values.put(key, v);
 				}
 			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		Repair rp=new Repair();
+		rp.values=new HashMap<String,String>();
+		rp.timeStamps=new HashMap<String,Date>();
+		rp.values.put("key1", "value1");
+		rp.values.put("key2", "value2");
+		rp.timeStamps.put("key1",new Date());
+		rp.timeStamps.put("key2", new Date());
+		System.out.println(rp.getAllKeyValueWithTime());
+		String s=rp.getAllKeyValueWithTime();
+		rp.values=new HashMap<String,String>();
+		rp.timeStamps=new HashMap<String,Date>();
+		rp.values.put("key2", "value3");
+		rp.timeStamps.put("key2", new Date());
+		ArrayList<String> kvarray=new ArrayList<String>();
+		kvarray.add(s);
+		try {
+			rp.repairKeyValueToLatest(kvarray);
+			System.out.println("Done");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
